73ae9b482bc690be6d47d4b551b0a7176e87a1fd
renrenrenz <durezarenz@gmail.com>
2024-11-15 16:22:37 +0800

    First Commit

---
+++ src/Flight.java
@@ -0,0 +1,14 @@
+public class Flight {
+    private int flightNo;
+    private String destination;
+    private String time;
+
+    Flight(int flightNo, String destination, String time) {
+        this.flightNo = flightNo;
+        this.destination = destination;
+        this.time = time;
+    }
+
+
+
+}
